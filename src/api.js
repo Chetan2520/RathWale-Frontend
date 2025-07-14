5c19f4ddd19d4798140b376cb709dd1a3c365f9d
Chetan Manker <chetanmanker67@gmail.com>
2025-07-14 19:45:38 +0530

    Add vercel rewrite config to fix 404 on refresh

--- src/api.js
+++ src/api.js
@@ -6,10 +6,10 @@ const API = axios.create({
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
-  if (token) {
+  if (token) { 
     req.headers.Authorization = `Bearer ${token}`;
   }
   return req;
 });
 
-export default API; 
+export default API;  
